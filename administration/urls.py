43d159115dcc09d76a2417796b9bfda2fe1ba233
Datonx <ankit.98.gupta.52@gmail.com>
2023-06-06 13:23:49 +0545

    Second commit: Students and Admin Apps initiated

---
+++ administration/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "administration"
+urlpatterns = [
+    path("dashboard/", views.dashboard, name="admin_dashboard")
+]
--- dilliraj_main/settings.py
+++ dilliraj_main/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "administration",
+    "students",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
