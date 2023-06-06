e01d36893a3e4b16bf4d9980ba80a10e0aa4d5ae
Datonx <ankit.98.gupta.52@gmail.com>
2023-06-06 18:48:54 +0545

    Third: About to add custom user_model

--- administration/urls.py
+++ administration/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "administration"
 urlpatterns = [
-    path("dashboard/", views.dashboard, name="admin_dashboard")
+    path("dashboard/", views.dashboard, name="admin_dashboard"),
+    path("pdf_view/", views.pdf_view, name="pdf_view")
 ]
--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
 # Create your views here.
 
 
 def dashboard(request):
     return render(request, "administration/dashboard.html")
+
+
+def pdf_view(request):
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(
+        100, 100, "This is the result for your child at Dilliraj Secondary School!")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename="hello.pdf")
